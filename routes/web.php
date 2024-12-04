58f582ccb466f10eb63eda8fdae756cfe71cc70a
mohame-ali57 <ma6646@fayoum.edu.eg>
2024-12-04 21:23:09 +0200

    Link Website Front-End

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Website\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,15 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::group(['prefix'=>'/auth'],function (){
-
-    Route::get('/login',[LoginController::class,'index'])->name('login');
-    Route::post('/login-post',[LoginController::class,'save'])->name('auth.login');
-});
-
-
-
-
-
-
+Route::get('/home',[HomeController::class,'home'])->name('website.home');
+Route::get('/about',[HomeController::class,'about'])->name('about');
+Route::get('/job-detail',[HomeController::class,'job_detail'])->name('job_detail');
+Route::get('/job-list',[HomeController::class,'job_list'])->name('job_list');
+Route::get('/job_category',[HomeController::class,'job_category'])->name('job_category');
+Route::get('/testimonial',[HomeController::class,'testimonial'])->name('testimonial');
+Route::get('/error_page',[HomeController::class,'error_page'])->name('error_page');
+Route::get('/contact',[HomeController::class,'contact'])->name('contact');
